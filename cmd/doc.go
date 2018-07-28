a103e363ce931b78e28ea2bd3f3aff6e398ad8be
Harry Scells <harryscells@gmail.com>
2018-07-28 16:02:30 +1000

    Merge cmd packages into boogie.

--- cmd/doc.go
+++
@@ -1,4 +0,0 @@
-// Package cmd contains command-line utilities that support groove in other ways than boogie. The utilities in this
-// package can be used to, for example, train SVM models for query chains. It also contains supporting code for these
-// utilities, such as loading queries from disk.
-package cmd
