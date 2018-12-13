2ff7e0ca7e14139555500e7eca5528c0dae6d6c5
Harry Scells <harryscells@gmail.com>
2018-12-13 11:36:03 +1000

    Add test for precision and recall evaluation.

---
+++ eval/precrec_test.go
@@ -0,0 +1,33 @@
+package eval_test
+
+import (
+	"github.com/hscells/groove/eval"
+	"github.com/hscells/trecresults"
+	"os"
+	"testing"
+)
+
+func TestPrecision(t *testing.T) {
+	f, err := os.OpenFile("testdata/positive_precision.results", os.O_RDONLY, 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+	results, err := trecresults.ResultsFromReader(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f, err = os.OpenFile("testdata/sigir2018medline.qrels", os.O_RDONLY, 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+	qrels, err := trecresults.QrelsFromReader(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	topic := "107"
+
+	l := results.Results[topic]
+	t.Log(eval.PrecisionEvaluator.Score(&l, qrels.Qrels[topic]))
+	t.Log(eval.RecallEvaluator.Score(&l, qrels.Qrels[topic]))
+}
